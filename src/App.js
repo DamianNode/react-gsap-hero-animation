f1b2e137b449fc066d274553566928a995f8cd78
Damian Janus <damian.janus@gmail.com>
2021-02-20 01:13:13 +0100

    Finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { TweenMax, TimelineLite, Power3 } from 'gsap';
 import { useEffect, useRef } from 'react';
 
-
 import 'reset-css';
 import './App.scss';
 
@@ -12,7 +11,8 @@ import imgGirl from './assets/image2.webp';
 const App = () => {
   const heroRef = useRef(null);
   const imagesRef = useRef(null);
-  const tl = new TimelineLite();
+  const contentRef = useRef(null);
+  const tl = new TimelineLite({delay: .8});
 
   useEffect(() => {
     TweenMax.to(heroRef.current, 0, { visibility: 'visible'})
@@ -20,18 +20,30 @@ const App = () => {
     const girlImage = imagesRef.current.firstElementChild;
     const manImage = imagesRef.current.lastElementChild;
 
-    tl.from(girlImage, 1.2, {y: 1280, ease: Power3.easeOut})
+    tl.from(girlImage, 1.2, {y: 1280, ease: Power3.easeOut}, 'Start')
       .from(girlImage.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut}, .2)
       .from(manImage, 1.2, {y: 1280, ease: Power3.easeOut}, .2)
       .from(manImage.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut}, .2)
-  })
+
+    const headlineFirst = contentRef.current.children[0].children[0];
+    const headlineSecond = headlineFirst.nextSibling;
+    const headlineThird =  headlineSecond.nextSibling;
+    const contentP = contentRef.current.children[1];
+    const contentButton = contentRef.current.children[2];
+
+    tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children], 1, {
+      y: 44, ease: Power3.easeOut, delay: .8
+    }, .15, 'Start')
+      .from(contentP, 1 , { y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
+      .from(contentButton, 1 , { y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
+  }, [tl])
 
   return (
     <div className="hero" ref={heroRef}>
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
-            <div className="hero-content-inner">
+            <div className="hero-content-inner" ref={contentRef}>
               <h1>
                 <div className="hero-content-line">
                   <div className="hero-content-line-inner">
